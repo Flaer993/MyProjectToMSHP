cdc95f749c729a1f5d5cdf0d286eb6e63428ddf9
Serega <schern329@gmail.com>
2021-04-11 16:37:21 +0300

    Add default tests at register page

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+from django.urls import reverse
+
+from core.forms import RegisterUserForm
+from core.views import RegisterUserView
+
+
+class RegisterPageTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.response = self.client.get(reverse('register_page'))
+
+    def test_index_response(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_index_context(self):
+        self.assertEqual(type(self.response.context['view']), RegisterUserView)
+        self.assertEqual(type(self.response.context['form']), RegisterUserForm)
+
+
+'''
+class LoginPageTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.response = self.client.get(reverse('login_page'))
+        
+    def test_index_redirect(self):
+        self.assertRedirects(self.response, '')'''
